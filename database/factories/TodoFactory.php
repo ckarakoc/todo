801cd87a7c8ec40fe01bbb7341f11179b3580ce8
okarakoc <okarakoc.mp@gmail.com>
2022-08-01 21:08:36 +0200

    Added model with seeders

---
+++ database/factories/TodoFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Todo;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Todo>
+ */
+class TodoFactory extends Factory
+{
+    protected $model = Todo::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->words(5, true),
+            'description' => $this->faker->sentence(45),
+            'status' => $this->faker->randomElement(['completed' ,'active', 'archived']),
+        ];
+    }
+}
